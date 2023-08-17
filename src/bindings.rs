14620f94f6821e6846124625903948d246ecff82
PTFOPlayer <patryk041103@gmail.com>
2023-08-17 18:48:12 +0200

    removed repeting code in rust part

--- src/bindings.rs
+++ src/bindings.rs
@@ -15,12 +15,12 @@ extern "C" {
     pub(crate) fn device_drm_render(dev_id: u32) -> ResultUint32T;
     pub(crate) fn device_subsystem_vendor_id(dev_id: u32) -> ResultUint16T;
     pub(crate) fn device_unique_id(dev_id: u32) -> ResultUint64T;
-    
+
     // pcie
     pub(crate) fn pci_bandwidth(dev_id: u32) -> ResultPcieBandwidth;
-    pub(crate) fn pcie_id(dev_id:u32) -> ResultUint64T;
-    pub(crate) fn topo_numa_affinity(dev_id:u32) -> ResultUint32T;
-    pub(crate) fn pci_throughput(dev_id:u32) -> ResultPcieThroughput;
+    pub(crate) fn pcie_id(dev_id: u32) -> ResultUint64T;
+    pub(crate) fn topo_numa_affinity(dev_id: u32) -> ResultUint32T;
+    pub(crate) fn pci_throughput(dev_id: u32) -> ResultPcieThroughput;
 
     // power
     pub(crate) fn power_sensor_count(dv_ind: u32) -> ResultUint16T;
@@ -28,8 +28,10 @@ extern "C" {
 
 use libc::c_char;
 
+use crate::error::RocmErr;
+
 #[repr(C)]
-pub(crate)struct ResultStr {
+pub(crate) struct ResultStr {
     pub(crate) status: u16,
     pub(crate) data: *mut c_char,
 }
@@ -67,4 +69,20 @@ pub(crate) struct ResultPcieThroughput {
     pub(crate) sent: u64,
     pub(crate) recived: u64,
     pub(crate) max_pkg_size: u64,
+}
+
+pub(crate) fn check_res(status: u16) -> Result<(), RocmErr> {
+    if status != 0 {
+        return Err(RocmErr::from_u16(status));
+    }
+    Ok(())
+}
+
+pub(crate) fn string_from_ptr(ptr: *mut i8) -> Result<String, RocmErr> {
+    let c_str = unsafe { std::ffi::CStr::from_ptr(ptr) };
+    let data = c_str.to_str().to_owned();
+    match data {
+        Ok(res) => Ok(res.to_owned()),
+        Err(_) => Err(RocmErr::RsmiStatusUnknownError),
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -20,6 +20,7 @@ impl RocmSmi {
         }
         Err(RocmErr::from_u16(code))
     }
+    
     pub fn into_first_device(self) -> Result<RocmSmiDevice, RocmErr> {
         RocmSmiDevice::new_from_rocm(self, 0)
     }
@@ -30,154 +31,79 @@ impl RocmSmi {
 
     pub fn get_device_count(&self) -> Result<u32, RocmErr> {
         let res = unsafe { num_devices() };
-
-        if res.status != 0 {
-            return Err(RocmErr::from_u16(res.status));
-        }
-
+        check_res(res.status)?;
         Ok(res.data)
     }
 
     pub fn get_device_id(&self, dev_id: u32) -> Result<u16, RocmErr> {
         let res = unsafe { device_id(dev_id) };
-
-        if res.status != 0 {
-            return Err(RocmErr::from_u16(res.status));
-        }
-
+        check_res(res.status)?;
         Ok(res.data)
     }
 
     pub fn get_device_name(&self, dev_id: u32) -> Result<String, RocmErr> {
         let res = unsafe { device_name(dev_id) };
-
-        if res.status != 0 {
-            return Err(RocmErr::from_u16(res.status));
-        }
-        let c_str = unsafe { std::ffi::CStr::from_ptr(res.data) };
-        let data = c_str.to_str().to_owned();
-        match data {
-            Ok(res) => Ok(res.to_owned()),
-            Err(_) => Err(RocmErr::RsmiStatusUnknownError),
-        }
+        check_res(res.status)?;
+        string_from_ptr(res.data)
     }
 
     pub fn get_device_vendor_id(&self, dev_id: u32) -> Result<u16, RocmErr> {
         let res = unsafe { device_vendor_id(dev_id) };
-
-        if res.status != 0 {
-            return Err(RocmErr::from_u16(res.status));
-        }
-
-        return Ok(res.data);
+        check_res(res.status)?;
+        Ok(res.data)
     }
 
     pub fn get_device_brand(&self, dev_id: u32) -> Result<String, RocmErr> {
         let res: ResultStr = unsafe { device_brand(dev_id) };
-
-        if res.status != 0 {
-            return Err(RocmErr::from_u16(res.status));
-        }
-        let c_str = unsafe { std::ffi::CStr::from_ptr(res.data) };
-        let data = c_str.to_str().to_owned();
-        match data {
-            Ok(res) => Ok(res.to_owned()),
-            Err(_) => Err(RocmErr::RsmiStatusUnknownError),
-        }
+        check_res(res.status)?;
+        string_from_ptr(res.data)
     }
 
     pub fn get_device_vendor_name(&self, dev_id: u32) -> Result<String, RocmErr> {
         let res: ResultStr = unsafe { device_vendor_name(dev_id) };
-
-        if res.status != 0 {
-            return Err(RocmErr::from_u16(res.status));
-        }
-        let c_str = unsafe { std::ffi::CStr::from_ptr(res.data) };
-        let data = c_str.to_str().to_owned();
-        match data {
-            Ok(res) => Ok(res.to_owned()),
-            Err(_) => Err(RocmErr::RsmiStatusUnknownError),
-        }
+        check_res(res.status)?;
+        string_from_ptr(res.data)
     }
     pub fn get_device_vram_vendor_name(&self, dev_id: u32) -> Result<String, RocmErr> {
         let res: ResultStr = unsafe { device_vram_vendor_name(dev_id) };
-
-        if res.status != 0 {
-            return Err(RocmErr::from_u16(res.status));
-        }
-        let c_str = unsafe { std::ffi::CStr::from_ptr(res.data) };
-        let data = c_str.to_str().to_owned();
-        match data {
-            Ok(res) => Ok(res.to_owned()),
-            Err(_) => Err(RocmErr::RsmiStatusUnknownError),
-        }
+        check_res(res.status)?;
+        string_from_ptr(res.data)
     }
 
     pub fn get_device_serial_number(&self, dev_id: u32) -> Result<String, RocmErr> {
         let res: ResultStr = unsafe { device_serial(dev_id) };
-
-        if res.status != 0 {
-            return Err(RocmErr::from_u16(res.status));
-        }
-        let c_str = unsafe { std::ffi::CStr::from_ptr(res.data) };
-        let data = c_str.to_str().to_owned();
-        match data {
-            Ok(res) => Ok(res.to_owned()),
-            Err(_) => Err(RocmErr::RsmiStatusUnknownError),
-        }
+        check_res(res.status)?;
+        string_from_ptr(res.data)
     }
 
     pub fn get_device_subsystem_id(&self, dev_id: u32) -> Result<u16, RocmErr> {
         let res = unsafe { device_subsystem_id(dev_id) };
-
-        if res.status != 0 {
-            return Err(RocmErr::from_u16(res.status));
-        }
+        check_res(res.status)?;
 
         return Ok(res.data);
     }
 
     pub fn get_device_subsystem_name(&self, dev_id: u32) -> Result<String, RocmErr> {
-        let res: ResultStr = unsafe { device_subsystem_name(dev_id) };
-
-        if res.status != 0 {
-            return Err(RocmErr::from_u16(res.status));
-        }
-        let c_str = unsafe { std::ffi::CStr::from_ptr(res.data) };
-        let data = c_str.to_str().to_owned();
-        match data {
-            Ok(res) => Ok(res.to_owned()),
-            Err(_) => Err(RocmErr::RsmiStatusUnknownError),
-        }
+        let res = unsafe { device_subsystem_name(dev_id) };
+        check_res(res.status)?;
+        string_from_ptr(res.data)
     }
 
     pub fn get_device_drm_render_minor(&self, dev_id: u32) -> Result<u32, RocmErr> {
         let res = unsafe { device_drm_render(dev_id) };
-
-        if res.status != 0 {
-            return Err(RocmErr::from_u16(res.status));
-        }
-
+        check_res(res.status)?;
         Ok(res.data)
     }
 
     pub fn get_device_subsystem_vendor_id(&self, dev_id: u32) -> Result<u16, RocmErr> {
         let res = unsafe { device_subsystem_vendor_id(dev_id) };
-
-        if res.status != 0 {
-            return Err(RocmErr::from_u16(res.status));
-        }
-
+        check_res(res.status)?;
         Ok(res.data)
     }
 
     pub fn get_device_unique_id(&self, dev_id: u32) -> Result<u64, RocmErr> {
         let res = unsafe { device_unique_id(dev_id) };
-
-        if res.status != 0 {
-            return Err(RocmErr::from_u16(res.status));
-        }
-
+        check_res(res.status)?;
         Ok(res.data)
     }
 
@@ -197,35 +123,26 @@ mod test {
                 let device_count = res.get_device_count();
                 match device_count {
                     Ok(count) => {
-                        println!("Device Count: {:?}", count);
-                        println!("Device ID: {:?}", res.get_device_id(0));
-                        println!("Device name: {:?}", res.get_device_name(0));
-                        println!("Device vendor id: {:?}", res.get_device_vendor_id(0));
-                        println!("Device brand: {:?}", res.get_device_brand(0));
-                        println!("Device vendor name: {:?}", res.get_device_vendor_name(0));
+                        println!("Count: {:?}", count);
+                        println!("ID: {:?}", res.get_device_id(0));
+                        println!("name: {:?}", res.get_device_name(0));
+                        println!("vendor id: {:?}", res.get_device_vendor_id(0));
+                        println!("brand: {:?}", res.get_device_brand(0));
+                        println!("vendor name: {:?}", res.get_device_vendor_name(0));
+                        println!("vram vendor name: {:?}", res.get_device_vram_vendor_name(0));
+                        println!("serial: {:?}", res.get_device_serial_number(0));
+                        println!("subsystem id: {:?}", res.get_device_subsystem_id(0));
+                        println!("subsystem name: {:?}", res.get_device_subsystem_name(0));
+                        println!("drm render minor: {:?}", res.get_device_drm_render_minor(0));
                         println!(
-                            "Device vram vendor name: {:?}",
-                            res.get_device_vram_vendor_name(0)
-                        );
-                        println!("Device serial: {:?}", res.get_device_serial_number(0));
-                        println!("Device subsystem id: {:?}", res.get_device_subsystem_id(0));
-                        println!(
-                            "Device subsystem name: {:?}",
-                            res.get_device_subsystem_name(0)
-                        );
-                        println!(
-                            "Device drm render minor: {:?}",
-                            res.get_device_drm_render_minor(0)
-                        );
-                        println!(
-                            "Device subsystem vendor id {:?}",
+                            "subsystem vendor id {:?}",
                             res.get_device_subsystem_vendor_id(0)
                         );
                         println!(
-                            "Device unique id (might fail if there is only one gpu) {:?}",
+                            "unique id (might fail if there is only one gpu) {:?}",
                             res.get_device_unique_id(0)
                         );
-                        println!("Device pcie data: {:?}", res.get_device_pcie_data(0));
+                        println!("pcie data: {:?}", res.get_device_pcie_data(0));
                     }
                     Err(err) => println!("{:?}", err),
                 }
@@ -237,14 +154,12 @@ mod test {
     #[test]
     fn device_test() {
         match RocmSmi::init() {
-            Ok(res) => {
-                match res.into_first_device() {
-                    Ok(dev) => {
-                        println!("succesfully got first device: {:?}", dev.get_brand());
-                    }
-                    Err(err) => println!("{:?}", err),
+            Ok(res) => match res.into_first_device() {
+                Ok(dev) => {
+                    println!("succesfully got first device: {:?}", dev.get_brand());
                 }
-            }
+                Err(err) => println!("{:?}", err),
+            },
             Err(err) => println!("{:?}", err),
         }
     }
