d5a23e0123af169229e06216196d1f9376c3101e
PTFOPlayer <patryk041103@gmail.com>
2023-09-02 11:22:56 +0200

    added fan speed reading

--- rsmi/queries/physical.h
+++ rsmi/queries/physical.h
@@ -12,52 +12,62 @@
 #include "./init.h"
 #endif
 
-result_uint16_t fan_sensors_count(uint32_t dv_ind)
+result_fans fans(uint32_t dv_ind)
 {
+    result_fans res = {0, 0, NULL, NULL, NULL};
     if (init.status != RSMI_STATUS_SUCCESS)
     {
-        result_uint16_t error = {init.status, 0};
-        return error;
+        res.status = init.status;
+        return res;
     }
-    int64_t speed;
+
+    int64_t *fan_rpm = (int64_t *)malloc(sizeof(int64_t));
+    int64_t *fan_speed = (int64_t *)malloc(sizeof(int64_t));
+    uint64_t *fan_cap = (uint64_t *)malloc(sizeof(uint64_t));
+
     uint16_t count = 0;
-    rsmi_status_t ret = rsmi_dev_fan_rpms_get(dv_ind, count, &speed);
-    while (ret == 0)
+
+    rsmi_status_t ret_rpm = rsmi_dev_fan_rpms_get(dv_ind, count, &fan_rpm[count]);
+    rsmi_status_t ret_speed = rsmi_dev_fan_speed_get(dv_ind, count, &fan_speed[count]);
+    rsmi_status_t ret_cap = rsmi_dev_fan_speed_max_get(dv_ind, count, &fan_cap[count]);
+
+    count++;
+
+    if (ret_rpm != RSMI_STATUS_SUCCESS)
     {
+        res.status = ret_rpm;
+        return res;
+    }
+    if (ret_speed != RSMI_STATUS_SUCCESS)
+    {
+        res.status = ret_speed;
+        return res;
+    }
+    if (ret_cap != RSMI_STATUS_SUCCESS)
+    {
+        res.status = ret_cap;
+        return res;
+    }
+
+    while (ret_rpm == ret_speed == ret_cap == 0)
+    {
+        ret_rpm = rsmi_dev_fan_rpms_get(dv_ind, count, &fan_rpm[count]);
+        ret_speed = rsmi_dev_fan_speed_get(dv_ind, count, &fan_speed[count]);
+        ret_cap = rsmi_dev_fan_speed_max_get(dv_ind, count, &fan_cap[count]);
+        if (ret_rpm != ret_speed != ret_cap != RSMI_STATUS_SUCCESS)
+            break;
+
         count++;
-        ret = rsmi_dev_fan_rpms_get(dv_ind, count, &speed);
+        fan_rpm = (int64_t *)realloc(fan_rpm, count * sizeof(int64_t));
+        fan_speed = (int64_t *)realloc(fan_speed, count * sizeof(int64_t));
+        fan_cap = (uint64_t *)realloc(fan_cap, count * sizeof(uint64_t));
     }
 
-    result_uint16_t res = {init.status, count};
-    return res;
-}
+    res.status = RSMI_STATUS_SUCCESS;
+    res.sensors = count;
+    res.fan_rpm_per_sensor = fan_rpm;
+    res.fan_speed_per_sensor = fan_speed;
+    res.max_fan_speed_per_sensor = fan_cap;
 
-result_int64_t fan_speed(uint32_t dv_ind, uint32_t sensor_ind)
-{
-    if (init.status != RSMI_STATUS_SUCCESS)
-    {
-        result_int64_t error = {init.status, 0};
-        return error;
-    }
-
-    int64_t speed;
-    rsmi_status_t ret = rsmi_dev_fan_speed_get(dv_ind, sensor_ind, &speed);
-
-    result_int64_t res = {ret, speed};
-    return res;
-}
-
-result_uint64_t max_fan_speed(uint32_t dv_ind, uint32_t sensor_ind)
-{
-    if (init.status != RSMI_STATUS_SUCCESS)
-    {
-        result_uint64_t error = {init.status, 0};
-        return error;
-    }
-
-    uint64_t speed;
-    rsmi_status_t ret = rsmi_dev_fan_speed_max_get(dv_ind, sensor_ind, &speed);
-
-    result_uint64_t res = {ret, speed};
     return res;
 }
