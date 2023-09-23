c409b6a35ef371e888e641d97694ef6562185802
Patryk <patryktofil03@gmail.com>
2023-09-23 18:30:36 +0200

    new build script

--- build.rs
+++ build.rs
@@ -1,11 +1,21 @@
 extern crate cmake;
+use std::env;
+
 use cmake::Config;
 
 fn main() {
-    let dst = Config::new("rsmi").very_verbose(true).build();
+    let dst = Config::new("rsmi")
+        .define("ROCM_DIR", "/opt/rocm")
+        .very_verbose(true)
+        .build();
     println!("cargo:rustc-link-search=native={}/build", dst.display());
-    println!("cargo:rustc-link-search=native=/opt/rocm/lib");
-    println!("cargo:rustc-link-search=native=/opt/rocm/lib64");
-    println!("cargo:rustc-link-lib=rocm_smi64");
-    println!("cargo:rustc-link-lib=rsmi64");
+
+    if !cfg!(feature = "vendored") {
+
+    } else {
+        println!("cargo:rustc-link-search=native=/opt/rocm/lib");
+        println!("cargo:rustc-link-search=native=/opt/rocm/lib64");
+        println!("cargo:rustc-link-lib=rocm_smi64");
+        println!("cargo:rustc-link-lib=rsmi64");
+    }
 }
